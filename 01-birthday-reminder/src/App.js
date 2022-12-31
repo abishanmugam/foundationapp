18020d2b5f0cf218a90e7768cf10ac0ede270941
Mukesh Selvaraj <abirami@ishamistygreen.com>
2022-12-31 23:32:52 +0530

    created 1.tours 2.reviews 3.acordion

--- 01-birthday-reminder/src/App.js
+++ 01-birthday-reminder/src/App.js
@@ -5,10 +5,12 @@ function App() {
   const [people, setPeople] = useState(data);
   return (
     <main>
+      {/* html section */}
       <section className="container">
         <h3>{people.length} birthdays today</h3>
         {/* Change Request - 9087 - JIRA#3498 */}
         <List people={people} />
+        {/* button click is calling setPeople function */}.
         <button onClick={() => setPeople([])}>clear all</button>
       </section>
     </main>
